175da42802781d6033fc3e3a99843c6755ff1138
Achyuth Chandra Annakula <achyuth.chandra@zemosolabs.com>
2020-06-30 18:25:44 +0530

    fix env vars

--- src/properties.ts
+++ src/properties.ts
@@ -1,6 +1,6 @@
 export const properties = {
   IFRAME_SECURE_SITE:
-    process.env.IFRAME_SECURE_SITE || "http://localhost:3040/iframe.html",
+    process.env.IFRAME_SECURE_SITE || "https://skyflow-js.s3-us-west-2.amazonaws.com/iframe.html",
   VERSION: process.env.npm_package_version || "0.1.1",
   CLIENT_URL:
     process.env.CLIENT_URL || "https://flogo.studio.skyflow.dev/getcreditscore",
