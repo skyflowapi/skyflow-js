06e0eebe97111d2cbc221ea896c3d1378baf99e6
prathyusha-skyflow <prathyusha.pallapothula@skyflow.com>
2021-08-24 10:11:27 +0530

    code cleanup

--- src_old/libs/deepClone.ts
+++
@@ -1,3 +0,0 @@
-export default function deepClone(json: Object): any {
-  return JSON.parse(JSON.stringify(json));
-}
--- src_old/libs/regex.ts
+++
@@ -1,7 +0,0 @@
-export const regExFromString = function (regExString: string) {
-  const lastSlash = regExString.lastIndexOf("/");
-  return new RegExp(
-    regExString.slice(1, lastSlash),
-    regExString.slice(lastSlash + 1)
-  );
-};
