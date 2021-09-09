101700a0e15ab76093797d4b78c1cb0f51790fd6
prathyusha-skyflow <prathyusha.pallapothula@skyflow.com>
2021-09-09 15:50:43 +0530

    add script for updating version

---
+++ .github/workflows/scripts/bump_version.sh
@@ -0,0 +1,14 @@
+echo "Bumping package version to $1.$2"
+
+Version=$1
+SEMVER=${Version:1}
+
+if [ -z $2 ]
+then
+	sed -E "s/version: .+/version: \"$SEMVER\"/g" package.json > tempfile && cat tempfile > package.json && rm -f tempfile
+else
+	sed -E "s/version: .+/version: \"$SEMVER-$2\"/g" package.json > tempfile && cat tempfile > package.json && rm -f tempfile
+fi
+
+echo --------------------------
+echo "Done, Package now at $1.$2"
