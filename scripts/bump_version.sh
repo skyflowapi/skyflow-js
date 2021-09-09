f9f1cb27b231b617ebb2a1a1d111c24245e0f882
prathyusha-skyflow <prathyusha.pallapothula@skyflow.com>
2021-09-09 16:51:14 +0530

    update scripts

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -5,9 +5,9 @@ SEMVER=${Version:1}
 
 if [ -z $2 ]
 then
-	sed -E "s/version: .+/version: \"$SEMVER\"/g" package.json > tempfile && cat tempfile > package.json && rm -f tempfile
+	sed -E "s/\"version\": .+/\"version\": \"$SEMVER\",/g" package.json > tempfile && cat tempfile > package.json && rm -f tempfile
 else
-	sed -E "s/version: .+/version: \"$SEMVER-$2\"/g" package.json > tempfile && cat tempfile > package.json && rm -f tempfile
+	sed -E "s/\"version\": .+/\"version\": \"$SEMVER-$2\",/g" package.json > tempfile && cat tempfile > package.json && rm -f tempfile
 fi
 
 echo --------------------------
