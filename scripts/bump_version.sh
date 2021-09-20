a4fd6b9b980a87e9137a39a426c6e7da34ff88c7
prathyusha-skyflow <prathyusha.pallapothula@skyflow.com>
2021-09-20 09:47:54 +0530

    update workflow files

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,14 +1,19 @@
-echo "Bumping package version to $1-$2"
-
 Version=$1
-SEMVER=${Version:1}
+SEMVER=$Version
 
 if [ -z $2 ]
 then
-	sed -E "s/\"version\": .+/\"version\": \"$SEMVER\",/g" package.json > tempfile && cat tempfile > package.json && rm -f tempfile
-else
-	sed -E "s/\"version\": .+/\"version\": \"$SEMVER-$2\",/g" package.json > tempfile && cat tempfile > package.json && rm -f tempfile
-fi
+	echo "Bumping package version to $1"
 
-echo --------------------------
-echo "Done, Package now at $SEMVER-$2"
+	sed -E "s/version: .+/version: \"$SEMVER\"/g" package.json > tempfile && cat tempfile > package.json && rm -f tempfile
+	echo --------------------------
+	echo "Done, Package now at $1"
+
+else
+	echo "Bumping package version to $1-dev.$2"
+
+	sed -E "s/version: .+/version: \"$SEMVER-$2\"/g" package.json > tempfile && cat tempfile > package.json && rm -f tempfile
+
+	echo --------------------------
+	echo "Done, Package now at $1-dev.$2"
+fi
