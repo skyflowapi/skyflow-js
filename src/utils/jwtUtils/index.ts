a1dd5548785c2d04767a4f54b490d730baa55bc8
prathyusha-skyflow <prathyusha.pallapothula@skyflow.com>
2022-02-24 14:44:20 +0530

    fix currentTime for checking token validity

--- src/utils/jwtUtils/index.ts
+++ src/utils/jwtUtils/index.ts
@@ -3,7 +3,8 @@ import jwt_decode, { JwtPayload } from 'jwt-decode';
 const isTokenValid = (token: string) => {
   let isJwtExpired = false;
   const decoded: JwtPayload = jwt_decode(token);
-  const currentTime = new Date().getTime() / 1000;
+  let currentTime = new Date().getTime() / 1000;
+  currentTime -= 300;
   const expiryTime = decoded.exp;
 
   if (expiryTime && currentTime > expiryTime) {
