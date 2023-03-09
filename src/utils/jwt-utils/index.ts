5285c332c2b68ea0b1494de94b2ea4a00b9f58e3
yaswanth-pula-skyflow <yaswanthsai.pula@skyflow.com>
2023-03-09 14:14:20 +0530

    SK-543 removal of grace period 5mins for cache refresh of bearer token.

--- src/utils/jwt-utils/index.ts
+++ src/utils/jwt-utils/index.ts
@@ -14,8 +14,7 @@ const isTokenValid = (token: string) => {
   }
   let isJwtExpired = false;
   const decoded: JwtPayload = jwt_decode(token);
-  let currentTime = new Date().getTime() / 1000;
-  currentTime += 300;
+  const currentTime = new Date().getTime() / 1000;
   const expiryTime = decoded.exp;
 
   if (expiryTime && currentTime > expiryTime) {
