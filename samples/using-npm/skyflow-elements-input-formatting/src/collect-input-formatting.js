8a2f04e71cec898839922c86d7c65c837320c660
yaswanth-pula-skyflow <yaswanthsai.pula@skyflow.com>
2023-05-03 22:03:23 +0530

    SK-556 update samples for input formatting in collect and reveal elements.

---
+++ samples/using-npm/skyflow-elements-input-formatting/src/collect-input-formatting.js
@@ -0,0 +1,139 @@
+/*
+  Copyright (c) 2023 Skyflow, Inc.
+*/
+import Skyflow from 'skyflow-js';
+
+try {
+
+  const skyflow = Skyflow.init({
+    vaultID: '<VAULT_ID>',
+    vaultURL: '<VAULT_URL>',
+    getBearerToken: () => {
+      return new Promise((resolve, reject) => {
+        const Http = new XMLHttpRequest();
+
+        Http.onreadystatechange = () => {
+          if (Http.readyState === 4 && Http.status === 200) {
+            const response = JSON.parse(Http.responseText);
+            resolve(response.accessToken);
+          }
+        };
+        const url = '<TOKEN_END_POINT_URL>';
+        Http.open('GET', url);
+        Http.send();
+      });
+    },
+    options: {
+      logLevel: Skyflow.LogLevel.ERROR,
+      env: Skyflow.Env.PROD,
+    }
+  });
+
+  // Create collect Container.
+  const collectContainer = skyflow.container(Skyflow.ContainerType.COLLECT);
+
+  // Custom styles for collect elements.
+  const collectStylesOptions = {
+    inputStyles: {
+      base: {
+        border: '1px solid #eae8ee',
+        padding: '10px 16px',
+        borderRadius: '4px',
+        color: '#1d1d1d',
+        marginTop: '4px',
+      },
+      complete: {
+        color: '#4caf50',
+      },
+      empty: {},
+      focus: {},
+      invalid: {
+        color: '#f44336',
+      },
+    },
+    labelStyles: {
+      base: {
+        fontSize: '16px',
+        fontWeight: 'bold',
+      },
+    },
+    errorTextStyles: {
+      base: {
+        color: '#f44336',
+      },
+    },
+  };
+
+  // Create collect elements.
+  const cardNumberElement = collectContainer.create({
+    table: 'pii_fields',
+    column: 'primary_card.card_number',
+    ...collectStylesOptions,
+    placeholder: 'card number',
+    label: 'Card Number',
+    type: Skyflow.ElementType.CARD_NUMBER,
+  }, {
+    format: 'XXXX-XXXX-XXXX-XXXX' // inbuilt format
+  });
+
+  const ssnElement = collectContainer.create({
+    table: 'pii_fields',
+    column: 'ssn',
+    ...collectStylesOptions,
+    label: 'SSN',
+    placeholder: 'ssn',
+    type: Skyflow.ElementType.INPUT_FIELD,
+  }, {
+    format: 'XXX-XX-XXXX',
+    translation: { X: '[0-9]' } // translates each 'X' in format string accepts a digit ranging from 0-9.
+  });
+
+  const expiryDateElement = collectContainer.create({
+    table: 'pii_fields',
+    column: 'primary_card.expiry_date',
+    ...collectStylesOptions,
+    label: 'Expiry Date',
+    placeholder: 'MM/YYYY',
+    type: Skyflow.ElementType.EXPIRATION_DATE,
+  }, {
+    format: 'MM/YYYY' // inbuilt format.
+  });
+
+  const passportNumberElement = collectContainer.create({
+    table: 'pii_fields',
+    column: 'passport_number',
+    ...collectStylesOptions,
+    label: 'Passport Number',
+    placeholder: 'passport number',
+    type: Skyflow.ElementType.INPUT_FIELD,
+  }, {
+    format: 'XXYYYYYYY',
+    translation: { X: '[A-Z]', Y: '[0-9]' }
+    // translates each 'X' in format string accepts a uppercase alphabet A to Z.
+    // and each 'Y' in format string accepts a digit ranging from 0-9.
+  });
+
+  // Mount the elements.
+  cardNumberElement.mount('#collectCardNumber');
+  ssnElement.mount('#collectCvv');
+  expiryDateElement.mount('#collectExpiryDate');
+  passportNumberElement.mount('#collectCardholderName');
+
+  // Collect all elements data.
+  const collectButton = document.getElementById('collectPCIData');
+  if (collectButton) {
+    collectButton.addEventListener('click', () => {
+      const collectResponse = collectContainer.collect();
+      collectResponse
+        .then((response) => {
+          document.getElementById('collectResponse').innerHTML =
+            JSON.stringify(response, null, 2);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  }
+} catch (err) {
+  console.log(err);
+}
