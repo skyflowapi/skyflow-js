0d40b13dbdba2599c1ef16d6c9eb26d72f067068
appasuamithsai <amithsai.appasu@zemosolabs.com>
2023-10-18 22:07:05 +0530

    SK-844 fix issue

--- src/metrics/index.ts
+++ src/metrics/index.ts
@@ -5,100 +5,100 @@ import { getMetaObject } from '../utils/helpers';
 export const METRIC_OBJECT: SharedMeticsObjectType = { records: [] };
 
 export function setBearerToken(metadata: any) {
-    metadata?.clientJSON?.config?.getBearerToken?.().then((authToken: string) => {
-      METRIC_OBJECT.bearerToken = authToken;
-    }).catch((err: any) => {
-      // eslint-disable-next-line no-console
-      console.error(err);
-    });
-  }
-  
-  export function initalizeMetricObject(metadata: any, elementId: string) {
-    setBearerToken(metadata);
-    const metaDataObject = getMetaObject(sdkDetails, metadata, navigator);
-    const elementMetricObject = {
-      element_id: elementId,
-      element_type: [],
-      div_id: '',
-      container_id: metadata?.uuid || '',
-      session_id: metadata?.session_id || '',
-      vault_id: metadata?.clientJSON?.config?.vaultID || '',
-      vault_url: metadata?.clientJSON?.config?.vaultURL || '',
-      events: [],
-      created_at: Date.now(),
-      region: Intl.DateTimeFormat().resolvedOptions().timeZone,
-      status: 'INITIALIZED',
-      sdk_name_version: metaDataObject.sdk_name_version,
-      sdk_client_device_model: metaDataObject.sdk_client_device_model,
-      sdk_client_os_details: metaDataObject.sdk_os_version,
-      sdk_runtime_details: metaDataObject.sdk_runtime_details,
-    };
-    METRIC_OBJECT.records.push(
-      elementMetricObject,
-    );
-  }
-  
-  export function updateMetricObjectValue(id: string, key: string, value: any) {
-    METRIC_OBJECT.records.forEach((event) => {
-      if (event.element_id === id) {
-        if (key === 'events' || key === 'element_type') {
-          event[key].push(value);
-        } else if (key === 'mount_end_time' && event.mount_start_time) {
-          event[key] = value;
-          event.latency = value - event.mount_start_time;
-        } else {
-          event[key] = value;
-        }
+  metadata?.clientJSON?.config?.getBearerToken?.().then((authToken: string) => {
+    METRIC_OBJECT.bearerToken = authToken;
+  }).catch((err: any) => {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  });
+}
+
+export function initalizeMetricObject(metadata: any, elementId: string) {
+  setBearerToken(metadata);
+  const metaDataObject = getMetaObject(sdkDetails, metadata, navigator);
+  const elementMetricObject = {
+    element_id: elementId,
+    element_type: [],
+    div_id: '',
+    container_id: metadata?.uuid || '',
+    session_id: metadata?.session_id || '',
+    vault_id: metadata?.clientJSON?.config?.vaultID || '',
+    vault_url: metadata?.clientJSON?.config?.vaultURL || '',
+    events: [],
+    created_at: Date.now(),
+    region: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    status: 'INITIALIZED',
+    sdk_name_version: metaDataObject.sdk_name_version,
+    sdk_client_device_model: metaDataObject.sdk_client_device_model,
+    sdk_client_os_details: metaDataObject.sdk_os_version,
+    sdk_runtime_details: metaDataObject.sdk_runtime_details,
+  };
+  METRIC_OBJECT.records.push(
+    elementMetricObject,
+  );
+}
+
+export function updateMetricObjectValue(id: string, key: string, value: any) {
+  METRIC_OBJECT.records.forEach((event) => {
+    if (event.element_id === id) {
+      if (key === 'events' || key === 'element_type') {
+        event[key].push(value);
+      } else if (key === 'mount_end_time' && event.mount_start_time) {
+        event[key] = value;
+        event.latency = value - event.mount_start_time;
+      } else {
+        event[key] = value;
       }
-    });
-  }
-  
-  export function getEventStatus(metricEvent: MeticsObjectType): string {
-    let status = metricEvent.status;
-    if (metricEvent.events.length === 0 || metricEvent.error) {
-      status = 'FAILED';
-    } else if (metricEvent.events.filter((element) => element.includes('MOUNTED')).length > 0) {
-      status = 'SUCCESS';
-    } else if (metricEvent.events.length > 0) {
-      status = 'PARTIAL_RENDER';
     }
-    return status;
+  });
+}
+
+export function getEventStatus(metricEvent: MeticsObjectType): string {
+  let status = metricEvent.status;
+  if (metricEvent.events.length === 0 || metricEvent.error) {
+    status = 'FAILED';
+  } else if (metricEvent.events.filter((element) => element.includes('MOUNTED')).length > 0) {
+    status = 'SUCCESS';
+  } else if (metricEvent.events.length > 0) {
+    status = 'PARTIAL_RENDER';
   }
-  
-  export function pushEventToMixpanel(elementId: string) {
-    const metricEvent = METRIC_OBJECT.records.filter((event) => event.element_id === elementId)[0];
-    metricEvent.status = getEventStatus(metricEvent);
-    const vaultURL = metricEvent.vault_url;
-    let eventPushed = false;
-    const event = {
-      event: metricEvent.container_id,
-      properties: {
-        ...metricEvent,
-        time: Math.floor(Date.now() / 1000),
-        distinct_id: metricEvent.container_id,
+  return status;
+}
+
+export function pushEventToMixpanel(elementId: string) {
+  const metricEvent = METRIC_OBJECT.records.filter((event) => event.element_id === elementId)[0];
+  metricEvent.status = getEventStatus(metricEvent);
+  const vaultURL = metricEvent.vault_url;
+  let eventPushed = false;
+  const event = {
+    event: metricEvent.container_id,
+    properties: {
+      ...metricEvent,
+      time: Math.floor(Date.now() / 1000),
+      distinct_id: metricEvent.container_id,
+    },
+  };
+
+  if (METRIC_OBJECT.bearerToken) {
+    fetch(`${vaultURL}/sdk/sdk-metrics`, {
+      method: 'POST',
+      body: JSON.stringify(event),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${METRIC_OBJECT.bearerToken}`,
       },
-    };
-  
-    if (METRIC_OBJECT.bearerToken) {
-      fetch(`${vaultURL}/sdk/sdk-metrics`, {
-        method: 'POST',
-        body: JSON.stringify(event),
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${METRIC_OBJECT.bearerToken}`,
-        },
+    })
+      .then((response: any) => {
+        eventPushed = response.data && (response.data >= 1);
       })
-        .then((response: any) => {
-          eventPushed = response.data && (response.data >= 1);
-        })
-        .catch((error) => {
-          eventPushed = error || eventPushed;
-        });
-    }
+      .catch((error) => {
+        eventPushed = error || eventPushed;
+      });
   }
-  
-  export function pushElementEventWithTimeout(elementID: string) {
-    setTimeout(() => {
-      pushEventToMixpanel(elementID);
-    }, 20000);
-  }
+}
+
+export function pushElementEventWithTimeout(elementID: string) {
+  setTimeout(() => {
+    pushEventToMixpanel(elementID);
+  }, 20000);
+}
--- tests/core/external/reveal/reveal-container.test.js
+++ tests/core/external/reveal/reveal-container.test.js
@@ -33,12 +33,13 @@ describe("Reveal Container Class", () => {
       off
     });
   });
+  const getBearerToken = jest.fn().mockImplementation(() => Promise.resolve());
   const testMetaData = {
     uuid: "123",
     config: {
       vaultID: "vault123",
       vaultURL: "sb.vault.dev",
-      getBearerToken: jest.fn(),
+      getBearerToken,
     },
     metaData: {
       clientDomain: "http://abc.com",
@@ -47,7 +48,7 @@ describe("Reveal Container Class", () => {
   const skyflowConfig = {
     vaultID: 'e20afc3ae1b54f0199f24130e51e0c11',
     vaultURL: 'https://testurl.com',
-    getBearerToken: jest.fn(),
+    getBearerToken,
   };
   
   const clientData = {
@@ -59,7 +60,7 @@ describe("Reveal Container Class", () => {
       context: { logLevel: LogLevel.ERROR,env:Env.PROD},
       config:{
         ...skyflowConfig,
-        getBearerToken:jest.fn().toString()
+        getBearerToken
       }
     } 
   }
@@ -152,8 +153,9 @@ describe("Reveal Container Class", () => {
     expect(onCbName).toBe(eventName);
     const onCb = on.mock.calls[1][1];
     onCb(data);
-
-    testRevealContainer.reveal();
+    testRevealContainer.reveal().catch(err => {
+      console.log(err)
+    });
     const emitEventName = emitSpy.mock.calls[1][0];
     const emitCb = emitSpy.mock.calls[1][2];
     expect(emitEventName).toBe(ELEMENT_EVENTS_TO_IFRAME.REVEAL_REQUEST+mockUuid);
@@ -266,7 +268,9 @@ describe("Reveal Container Class", () => {
       containerId:mockUuid
     }
     
-    testRevealContainer.reveal();
+    testRevealContainer.reveal().catch(err => {
+      console.log(err);
+    });
     const eventName = ELEMENT_EVENTS_TO_CONTAINER.ELEMENT_MOUNTED+mockUuid
     bus.emit(eventName,data);
     const onCbName = on.mock.calls[1][0];
