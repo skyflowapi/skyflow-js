584d13314a08f8680485f4fa255e328193aa026a
appasuamithsai <amithsai.appasu@zemosolabs.com>
2023-10-21 14:56:02 +0530

    SK-844 change origin

--- src/core/external/collect/collect-element.ts
+++ src/core/external/collect/collect-element.ts
@@ -7,6 +7,8 @@ import {
   ELEMENT_EVENTS_TO_CONTAINER,
   ELEMENT_EVENTS_TO_IFRAME,
   ELEMENTS,
+  EVENT_TYPES,
+  METRIC_TYPES,
 } from '../../constants';
 import EventEmitter from '../../../event-emitter';
 import Bus from '../../../libs/bus';
@@ -113,13 +115,14 @@ class CollectElement extends SkyflowElement {
     this.elementType = this.#isSingleElementAPI
       ? this.#elements[0].elementType
       : 'group';
+
     initalizeMetricObject(metaData, elementId);
     if (!this.#isSingleElementAPI) {
-      updateMetricObjectValue(this.#elementId, 'element_type', this.elementType);
+      updateMetricObjectValue(this.#elementId, METRIC_TYPES.ELEMENT_TYPE_KEY, this.elementType);
     }
     this.#states = [];
     this.#elements.forEach((element) => {
-      updateMetricObjectValue(this.#elementId, 'element_type', element.elementType);
+      updateMetricObjectValue(this.#elementId, METRIC_TYPES.ELEMENT_TYPE_KEY, element.elementType);
       this.#states.push({
         isEmpty: true,
         isComplete: false,
@@ -154,17 +157,17 @@ class CollectElement extends SkyflowElement {
 
     this.#bus.on(ELEMENT_EVENTS_TO_CLIENT.MOUNTED, (data) => {
       if (container.type === ContainerType.COMPOSABLE) {
-        updateMetricObjectValue(this.#elementId, 'mount_end_time', Date.now());
+        updateMetricObjectValue(this.#elementId, METRIC_TYPES.MOUNT_END_TIME, Date.now());
         this.#elements.forEach((element) => {
           if (data.name === element.elementName) {
-            updateMetricObjectValue(this.#elementId, 'events', `${element.elementType}_MOUNTED`);
+            updateMetricObjectValue(this.#elementId, METRIC_TYPES.EVENTS_KEY, `${element.elementType}_${METRIC_TYPES.EVENTS.MOUNTED}`);
             element.isMounted = true;
             this.#mounted = true;
           }
         });
       } else if (data.name === this.#elements[0].elementName) {
-        updateMetricObjectValue(this.#elementId, 'mount_end_time', Date.now());
-        updateMetricObjectValue(this.#elementId, 'events', 'MOUNTED');
+        updateMetricObjectValue(this.#elementId, METRIC_TYPES.MOUNT_END_TIME, Date.now());
+        updateMetricObjectValue(this.#elementId, METRIC_TYPES.EVENTS_KEY, EVENT_TYPES.MOUNTED);
         this.#elements[0].isMounted = true;
         this.#mounted = true;
       }
@@ -187,7 +190,7 @@ class CollectElement extends SkyflowElement {
           this.#iframe.setIframeHeight(payload.height);
         });
     });
-    updateMetricObjectValue(this.#elementId, 'div_id', domElement);
+    updateMetricObjectValue(this.#elementId, METRIC_TYPES.DIV_ID, domElement);
     pushElementEventWithTimeout(this.#elementId);
     const sub = (data, callback) => {
       if (data.name === this.#iframe.name) {
@@ -202,14 +205,14 @@ class CollectElement extends SkyflowElement {
                 ...this.#states[index],
                 elementType: element.elementType,
               });
-              updateMetricObjectValue(this.#elementId, 'events', `${element.elementType}_FRAME_READY`);
+              updateMetricObjectValue(this.#elementId, METRIC_TYPES.EVENTS_KEY, `${element.elementType}_${METRIC_TYPES.EVENTS.READY}`);
             }
           });
         } else {
           this.#eventEmitter._emit(ELEMENT_EVENTS_TO_CLIENT.READY, {
             ...elementState,
           });
-          updateMetricObjectValue(this.#elementId, 'events', 'FRAME_READY');
+          updateMetricObjectValue(this.#elementId, METRIC_TYPES.EVENTS_KEY, EVENT_TYPES.READY);
         }
 
         this.#bus.off(
@@ -228,17 +231,17 @@ class CollectElement extends SkyflowElement {
     if (isComposable) {
       this.#iframe.mount(domElement, this.#elementId);
       this.#bus.on(ELEMENT_EVENTS_TO_IFRAME.FRAME_READY + this.containerId, sub);
-      updateMetricObjectValue(this.#elementId, 'mount_start_time', Date.now());
+      updateMetricObjectValue(this.#elementId, METRIC_TYPES.MOUNT_START_TIME, Date.now());
     } else {
       if (this.#readyToMount) {
         this.#iframe.mount(domElement, this.#elementId);
         this.#bus.on(ELEMENT_EVENTS_TO_IFRAME.FRAME_READY + this.containerId, sub);
-        updateMetricObjectValue(this.#elementId, 'mount_start_time', Date.now());
+        updateMetricObjectValue(this.#elementId, METRIC_TYPES.MOUNT_START_TIME, Date.now());
         return;
       }
       this.#groupEmitter?.on(ELEMENT_EVENTS_TO_CONTAINER.COLLECT_CONTAINER_MOUNTED, (data) => {
         if (data?.containerId === this.containerId) {
-          updateMetricObjectValue(this.#elementId, 'mount_start_time', Date.now());
+          updateMetricObjectValue(this.#elementId, METRIC_TYPES.MOUNT_START_TIME, Date.now());
           this.#iframe.mount(domElement, this.#elementId);
           this.#bus.on(ELEMENT_EVENTS_TO_IFRAME.FRAME_READY + this.containerId, sub);
         }
