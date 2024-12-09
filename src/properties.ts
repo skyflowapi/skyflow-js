0c22ed39c323c59f8ab3fa81bb0fd8f3b1aaef01
“amith-skyflow” <amit@skyflow.com>
2024-12-10 00:03:12 +0530

    SK-1611: fix typo

--- src/properties.ts
+++ src/properties.ts
@@ -2,8 +2,8 @@
 Copyright (c) 2022 Skyflow, Inc.
 */
 const properties = {
-  IFRAME_SECURE_SITE: `${process.env.IFRAME_SECURE_ORGIN}/${process.env.IFRAME_SECURE_SITE}`,
-  IFRAME_SECURE_ORGIN: process.env.IFRAME_SECURE_ORGIN || '',
+  IFRAME_SECURE_SITE: `${process.env.IFRAME_SECURE_ORIGIN}/${process.env.IFRAME_SECURE_SITE}`,
+  IFRAME_SECURE_ORIGIN: process.env.IFRAME_SECURE_ORIGIN || '',
 };
 
 export default properties;
