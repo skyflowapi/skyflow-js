2b9d8c9f54c2790ed99efa8c32c21f00636a04b3
skyflow-shashank-prajapati <140067931+skyflow-shashank-prajapati@users.noreply.github.com>
2025-01-08 11:56:51 +0530

    FINT-664:get browser details 3DS (#516)

---
+++ samples/using-npm/3ds-helper-functions/src/index.js
@@ -0,0 +1,41 @@
+/*
+  Copyright (c) 2022 Skyflow, Inc.
+*/
+
+import Skyflow from 'skyflow-js';
+
+try {
+  // getBrowserDetails for 3DS.
+  const actionButtons = document.getElementById("actionButtons")
+  const getDetailsButton = document.getElementById("getBrowserDetails");
+  if (getDetailsButton) {
+    getDetailsButton.addEventListener("click", () => {
+      const browserDetails = Skyflow.ThreeDS.getBroswerDetails();
+      document.getElementById("threeDSBrowserDetails").innerHTML =
+        JSON.stringify(browserDetails);
+    });
+    //showChallenge for 3DS
+    const redirectButton = document.getElementById("redirectToChallenge");
+    if (redirectButton) {
+      redirectButton.addEventListener("click", () => {
+        const challengeWindow = document.getElementById("challengeWindow");
+        const challengeIFrame = Skyflow.ThreeDS.showChallenege(
+          "<acs-url>",
+          "<c-req>",
+          "04",
+          challengeWindow
+        );
+        challengeIFrame.addEventListener("load", function () {
+          if(actionButtons.hidden){
+            challengeIFrame.hidden = true
+            actionButtons.hidden = false
+          } else{
+            actionButtons.hidden = true
+          }
+        });
+      });
+    }
+  }
+} catch (err) {
+  console.log(err);
+}
