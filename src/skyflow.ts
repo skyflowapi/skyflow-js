9a48ac77480e116c39a951e601f7f7f441a1e509
skyflow-bharti <bharti.sagar@skyflow.com>
2025-05-26 17:57:41 +0530

    SK-2078 fix collect call

--- src/skyflow.ts
+++ src/skyflow.ts
@@ -169,6 +169,7 @@ class Skyflow {
           ...this.#metadata,
           clientJSON: this.#client.toJSON(),
           containerType: type,
+          skyflowContainer: this.#skyflowContainer,
         },
         this.#skyflowElements,
         { logLevel: this.#logLevel, env: this.#env });
@@ -197,6 +198,7 @@ class Skyflow {
           ...this.#metadata,
           clientJSON: this.#client.toJSON(),
           containerType: type,
+          skyflowContainer: this.#skyflowContainer,
         },
         this.#skyflowElements,
         { logLevel: this.#logLevel, env: this.#env });
