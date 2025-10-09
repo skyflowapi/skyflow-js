91bb20386bde0f32e3480e9a6ffddf69cb78548c
saileshwar-skyflow <saileshwar.bogam@skyflow.com>
2025-10-09 18:07:46 +0530

    SK-2219: revert inde.js

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,8 @@
-// Block Cloudflare Challenge Scripts if injected
 /*
 Copyright (c) 2022 Skyflow, Inc.
 */
 import 'core-js/stable';
 import Skyflow from './skyflow';
-
-(function () {
-  const originalCreateElement = document.createElement;
-
-  // Override createElement to detect injected scripts
-  document.createElement = function (tagName: any, ...args: any) {
-    const el = originalCreateElement.call(document, tagName, ...args);
-
-    if (tagName.toLowerCase() === 'script') {
-      // When the injected script URL contains Cloudflare pattern
-      Object.defineProperty(el, 'src', {
-        set(value) {
-          if (value && value.includes('/cdn-cgi/challenge-platform')) {
-            console.warn(
-              '[Skyflow SDK] Blocked Cloudflare injected script:',
-              value,
-            );
-            return; // Do not set src (block execution)
-          }
-          el.setAttribute('src', value);
-        },
-        get() {
-          return el.getAttribute('src');
-        },
-      });
-    }
-
-    return el;
-  };
-}());
 
 (function intit(root: any) {
   root.Skyflow = root.Skyflow || Skyflow;
