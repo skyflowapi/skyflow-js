e35dec0a5beefe0dab49ebc5379766894f0f1eaa
skyflow-bharti <bharti.sagar@skyflow.com>
2025-11-25 20:40:02 +0530

    SK-2408 test the different files for element type

--- src/properties.ts
+++ src/properties.ts
@@ -2,7 +2,7 @@
 Copyright (c) 2022 Skyflow, Inc.
 */
 const properties = {
-  IFRAME_SECURE_SITE: `${process.env.IFRAME_SECURE_ORIGIN}/${process.env.IFRAME_SECURE_SITE}`,
+  IFRAME_SECURE_SITE: process.env.IFRAME_SECURE_ORIGIN,
   IFRAME_SECURE_ORIGIN: process.env.IFRAME_SECURE_ORIGIN || '',
 };
 
