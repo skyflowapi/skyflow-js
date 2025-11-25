28eea796305a79c52224b1bf493e9e954e9f2452
skyflow-bharti <bharti.sagar@skyflow.com>
2025-11-25 20:32:21 +0530

    SK-2408 test the different files for element type

--- src/properties.ts
+++ src/properties.ts
@@ -2,10 +2,8 @@
 Copyright (c) 2022 Skyflow, Inc.
 */
 const properties = {
-  IFRAME_SECURE_SITE: 'http://js.skyflow.dev/', // Default monolithic
-  IFRAME_SECURE_ORIGIN: 'http://js.skyflow.dev',
-  // For split bundles in dev, the base path would be 'http://localhost:3040/'
-  // and getIframeSrcByType will append the specific HTML file
+  IFRAME_SECURE_SITE: `${process.env.IFRAME_SECURE_ORIGIN}/${process.env.IFRAME_SECURE_SITE}`,
+  IFRAME_SECURE_ORIGIN: process.env.IFRAME_SECURE_ORIGIN || '',
 };
 
 export default properties;
