9b219149dbcbfa70011a605b5deda0b5caaa8259
skyflow-bharti <bharti.sagar@skyflow.com>
2026-03-12 09:39:56 +0530

    SK-2610 fix the type gap to pass the nested objects

--- src/utils/common/index.ts
+++ src/utils/common/index.ts
@@ -321,7 +321,7 @@ export interface InputStyles extends ErrorTextStyles {
 export interface CollectElementOptions {
   required?: boolean,
   format?: string,
-  translation?: Record<string, string>,
+  translation?: Record<string, Style>,
   enableCardIcon?: boolean,
   enableCopy?: boolean,
   cardMetadata?: CardMetadata,
